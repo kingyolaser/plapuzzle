b474d9343d0e9df525cddbed25cfa3b020a1b930
kingyolaser <kingyolaser@gmail.com>
2018-01-07 20:13:25 +0900

    developing

--- plapuzzle.cpp
+++ plapuzzle.cpp
@@ -4,25 +4,54 @@
 
 enum{
 	BLANK = 0,
-	WALL  = 1,
+	WALL  = 99,
 };
 
 #define SIZE 8
 
 
 /**************************************************************************/
-typedef struct Piece {
+typedef struct XY_ {
+	int x;
+	int y;
+} XY;
+/**************************************************************************/
+typedef struct Shape_ {
+	XY  dot[6];
+} Shape;
+/**************************************************************************/
+typedef struct Piece_ {
 	int shapes;
-	char map[10][18];
+	Shape shape[8];
 }Piece;
 
 /**************************************************************************/
 Piece piece_z = {
-	1,
+	4,
+
 	{
-		{1,1},
-		{0,1},
-		{0,1,1}
+		// ■■
+		//   ■
+		//   ■■
+		{{{0,0},{1,0},{1,1},{1,2},{2,2},{99,99}}},
+
+		
+		//     ■
+		// ■■■
+		// ■
+
+		{{{0,0},{0,1},{-1,1},{-2,1},{-2,2},{99,99}}},
+
+		// ■
+		// ■■■
+		//     ■
+		{{{0,0},{0,1},{1,1},{2,1},{2,2},{99,99}}},
+
+		//   ■■
+		//   ■
+		// ■■
+		{{{0,0},{1,0},{0,1},{0,2},{-1,2},{99,99}}},
+	
 	}
 };
 
@@ -34,29 +63,39 @@ public:
 	Ban(){
 		memset( &map[0][0], WALL, sizeof(map) );
 		
-		for( int y=4; y<4+SIZE; y++ ){
-			for( int x=4; x<4+SIZE; x++ ){
-				map[y][x]=BLANK;
+		for( int y=1; y<1+SIZE; y++ ){
+			for( int x=1; x<1+SIZE; x++ ){
+				map[x][y]=BLANK;
 			}
 		}
 		
 	}
 	
+	
 	void print(){
-		for( int y=4; y<4+SIZE; y++ ){
-			for( int x=4; x<4+SIZE; x++ ){
-				if( map[y][x]==BLANK ){
+		for( int y=1; y<1+SIZE; y++ ){
+			for( int x=1; x<1+SIZE; x++ ){
+				if( map[x][y]==BLANK ){
 					printf(". ");
 				}else{
-					printf("%c ", map[y][x]+'a');
+					printf("%c ", map[x][y]+'a');
 				}
 			}
 			puts("");
 		}
 	}
 	
-	void put(Piece a){
-		
+	void put(int x, int y, Shape s){
+		//printf("putting test\n");
+		for( int i=0; i<5; i++ ){
+			if( map[x+s.dot[i].x][y+s.dot[i].y] != BLANK ){
+				printf("i=%d, x=%d, y=%d, map=%d\n", i, x,y,map[x+s.dot[i].x][y+s.dot[i].y] );
+				printf("%d %d\n", s.dot[i].x, s.dot[i].y );
+				exit(1);
+			}
+			
+			map[x+s.dot[i].x][y+s.dot[i].y] = 2;
+		}
 	}
 };
 
@@ -65,9 +104,17 @@ int main()
 {
 	Ban b;
 	
-	b.put(4,4,piece_z);
 	
+	Piece ps[12]={piece_z};
+
+	b.put(1,1,piece_z.shape[0]);
 	
-    printf("hello.\n");
+	int used_map[12] = {0};
+	for( int p=0; p<12; p++ ){
+		
+	}
+	
+	b.print();
+	
 }
 
